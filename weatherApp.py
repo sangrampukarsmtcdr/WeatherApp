ffd1642cc138fabaada0e932b263721c3375715f
Sumit Sangrampurkar <114391567+sangrampukarsmtcdr@users.noreply.github.com>
2023-06-30 18:41:45 +0530

    Added error catching function

--- weatherApp.py
+++ weatherApp.py
@@ -2,16 +2,19 @@ import requests
 import json
 
 def weatherTemp():
-    print("--------- WELCOME ---------\n")
-    city = input("Enter city name:")
+    try:
+        print("--------- WELCOME ---------\n")
+        city = input("[+]Enter city name:")
 
-    url = f"https://api.weatherapi.com/v1/current.json?key=70d9f075f5cb4fa8b2b113745232706&q={city}&aqi=no" # url got from api dashboard 
+        url = f"https://api.weatherapi.com/v1/current.json?key=70d9f075f5cb4fa8b2b113745232706&q={city}&aqi=no" # url got from api dashboard 
 
-    r = requests.get(url)
+        r = requests.get(url)
 
-    wDic = json.loads(r.text)
+        wDic = json.loads(r.text)
 
-    temp = wDic["current"]["temp_c"]
-    print(f"Temperature in {city} is {temp} degree celcius")
+        temp = wDic["current"]["temp_c"]
+        print(f"[*] Temperature in {city} is {temp} degree celcius")
+    except KeyError:
+        print("[!] There is no such city.")
 
 weatherTemp()
